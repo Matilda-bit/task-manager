d4efaee877ffe73d4f7a6a1c50e1388aab68f458
Matilda-bit <polinaovras@gmail.com>
2024-10-20 20:26:53 +0300

    redux

---
+++ src/redux/taskSlice.ts
@@ -0,0 +1,39 @@
+// src/redux/taskSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Task } from '../types/Task';
+
+interface TaskState {
+  tasks: Task[];
+}
+
+const initialState: TaskState = {
+  tasks: [],
+};
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask(state, action: PayloadAction<Task>) {
+      state.tasks.push(action.payload);
+    },
+    editTask(state, action: PayloadAction<Task>) {
+      const index = state.tasks.findIndex((task) => task.id === action.payload.id);
+      if (index !== -1) {
+        state.tasks[index] = action.payload;
+      }
+    },
+    deleteTask(state, action: PayloadAction<number>) {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
+    toggleComplete(state, action: PayloadAction<number>) {
+      const task = state.tasks.find((task) => task.id === action.payload);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+  },
+});
+
+export const { addTask, editTask, deleteTask, toggleComplete } = taskSlice.actions;
+export default taskSlice.reducer;
